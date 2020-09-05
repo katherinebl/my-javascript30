a52cbdc5cb137486d684e70ada9120f3b81a4a07
katherinebl <kath.gbl@gmail.com>
2020-09-05 13:00:53 +0200

    Added styles when keypressed

--- 01-javascript-drum-kit/main.js
+++ 01-javascript-drum-kit/main.js
@@ -2,7 +2,10 @@
 
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.which}"]`)
+    const key = document.querySelector(`.key[data-key="${e.which}"]`)
+  
     if(!audio) return //stops function 
     audio.currentTime = 0 //rewind to the start 
     audio.play()
+    key.classList.add('playing')
 }) 
