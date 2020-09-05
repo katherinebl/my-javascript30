7ce0515f8d5dd654332a7374725ade2ef63fc267
katherinebl <kath.gbl@gmail.com>
2020-09-05 13:23:07 +0200

    Finished exercise

--- 01-javascript-drum-kit/main.js
+++ 01-javascript-drum-kit/main.js
@@ -1,11 +1,21 @@
+'use strict';
 
+function playSound(e) { 
+  const audio = document.querySelector(`audio[data-key="${e.which}"]`)
+  const key = document.querySelector(`.key[data-key="${e.which}"]`)
 
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.which}"]`)
-    const key = document.querySelector(`.key[data-key="${e.which}"]`)
-  
-    if(!audio) return //stops function 
-    audio.currentTime = 0 //rewind to the start 
-    audio.play()
-    key.classList.add('playing')
-}) 
+  if(!audio) return //stops function 
+  audio.currentTime = 0 //rewind to the start 
+  audio.play()
+  key.classList.add('playing')
+    
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'box-shadow') return //skip if it's not included
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key'); // retrurns a node
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound) 
