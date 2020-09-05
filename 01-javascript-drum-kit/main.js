b134e4df7794cc259039d541a640e65f5133ef9d
katherinebl <kath.gbl@gmail.com>
2020-09-05 12:32:16 +0200

    Now the audio plays as many times as you press the key without delay

--- 01-javascript-drum-kit/main.js
+++ 01-javascript-drum-kit/main.js
@@ -3,5 +3,6 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.which}"]`)
     if(!audio) return //stops function 
+    audio.currentTime = 0 //rewind to the start 
     audio.play()
 }) 
