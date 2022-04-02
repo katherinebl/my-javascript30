3017a5d5ab937f4e3878da3e39cd23ddb2da7e84
katherinebl <kath.gbl@gmail.com>
2022-04-02 23:25:32 +0200

    Added minutes and hours

--- 02-javascript-clock/main.js
+++ 02-javascript-clock/main.js
@@ -1,14 +1,32 @@
 "use strict";
 
 const secondNeedle = document.querySelector(".seconds");
+const minutesNeedle = document.querySelector(".minutes");
+const hourNeedle = document.querySelector(".hours");
 
 function setDate() {
   const now = new Date();
-  const seconds = now.getSeconds();
-  const secondDegrees = (seconds / 60) * 360 + 90;
-  secondNeedle.style.transform = `rotate(${secondDegrees}deg)`;
 
-  console.log("seconds: ", seconds);
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondNeedle.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360;
+  minutesNeedle.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360;
+  hourNeedle.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  console.log(
+    "now: ",
+    now.getHours(),
+    ":",
+    now.getMinutes(),
+    ":",
+    now.getSeconds()
+  );
 }
 
 setInterval(() => {
