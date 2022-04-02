c2fed120d49c934607c7901e2afa784571b18900
katherinebl <kath.gbl@gmail.com>
2022-04-02 22:35:40 +0200

    Added seconds

--- 02-javascript-clock/main.js
+++ 02-javascript-clock/main.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("HI");
+const secondNeedle = document.querySelector(".seconds");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondNeedle.style.transform = `rotate(${secondDegrees}deg)`;
+
+  console.log("seconds: ", seconds);
+}
+
+setInterval(() => {
+  setDate();
+}, 1000);
