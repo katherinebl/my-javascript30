c77012e41bc70599f6d039deb927db4262acb05b
katherinebl <kath.gbl@gmail.com>
2022-04-07 23:09:53 +0200

    Finished updating CSS variables with JS

--- 03-css-variables-and-js/main.js
+++ 03-css-variables-and-js/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleChange() {
+  //dataset is a tidy object that contains all the data-attributes that we add to the html elements
+
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleChange));
+inputs.forEach((input) => input.addEventListener("mousemove", handleChange));
